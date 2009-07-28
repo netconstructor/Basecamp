f21fcf9f061a8e69902755850b74cfc5356f7322
Naga Gangadhar Reddy <ganga@ganga-laptop.(none)>
2009-07-28 16:11:45 +0530

    added date initialiser

--- public/javascripts/basecamp/milestones.js
+++ public/javascripts/basecamp/milestones.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+	var currentTime = new Date()
+        var month = currentTime.getMonth() + 1
+        if(month<10)
+        {
+           month = "0"+month;
+        }
+        var day = currentTime.getDate()
+        var year = currentTime.getFullYear()
+        $("#MilestoneDate").html( day + "/" + month + "/" + year)
+
 	$("#datepicker").datepicker({
 		dateFormat: 'dd-mm-yy',
 		onSelect: function(dateText, inst)
