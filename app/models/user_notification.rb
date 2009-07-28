69ae73c42cfa38127af5275b2762072f03de9c03
Naga Gangadhar Reddy <ganga@ganga-laptop.(none)>
2009-07-28 19:50:47 +0530

    modified logout link

--- app/models/user_notification.rb
+++ app/models/user_notification.rb
@@ -1,7 +1,12 @@
-class UserNotification < ActiveRecord::Base
-
-  def todo_notify(options)
-       self.generic_options(options)
+class UserNotification < ActionMailer::Base
+  def todo_notify(todo)
+         recipients activation.email_address
+         from "Basecamp <notifications@pagalguy.com>"
+         subject "You have been assigned a todo"
+         sent_on Time.now
+         body :todo => todo
   end
+
+
   
 end
