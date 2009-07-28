acdf636b0dfc612f8f9e4a6c2e097c381d12a4d9
Naga Gangadhar Reddy <ganga@ganga-laptop.(none)>
2009-07-28 18:28:00 +0530

    added todo notification

--- app/models/activation_mailer.rb
+++ app/models/activation_mailer.rb
@@ -4,6 +4,6 @@ class ActivationMailer < ActionMailer::Base
          from "Basecamp <notifications@pagalguy.com>"
          subject "Basecamp Activation Link"  
          sent_on Time.now 
-         body {:activation => activation} 
+         body :activation => activation 
     end
 end
